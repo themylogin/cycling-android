3570ec1ca3593e883ed23f6d67c8c3c46f1a6897
themylogin <themylogin@gmail.com>
2017-12-04 18:34:20 +0300

    ...

--- app/src/main/java/com/example/themylogin/cycling/MapsActivity.java
+++ app/src/main/java/com/example/themylogin/cycling/MapsActivity.java
@@ -1,5 +1,8 @@
 package com.example.themylogin.cycling;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -20,13 +23,22 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.ByteArrayOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Queue;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.json.JSONObject;
@@ -39,7 +51,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Marker me;
     private Marker he;
 
-    private Queue<String> outputQueue = new ConcurrentLinkedQueue<String>();
+    private Queue<byte[]> outputQueue = new ConcurrentLinkedQueue<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,16 +62,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        final Queue<String> outputQueue = this.outputQueue;
+        final Queue<byte[]> outputQueue = this.outputQueue;
 
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 try {
-                    JSONObject data = new JSONObject();
-                    data.put("latitude", location.getLatitude());
-                    data.put("longitude", location.getLongitude());
-                    outputQueue.add(data.toString());
+                    byte[] latitude = ByteBuffer.allocate(4).putFloat((float)location.getLatitude()).array();
+                    byte[] longitude = ByteBuffer.allocate(4).putFloat((float)location.getLatitude()).array();
+                    byte[] speed = ByteBuffer.allocate(4).putFloat(location.getSpeed()).array();
+                    outputQueue.add(new byte[]{
+                            0x01,                   // Send packet
+                            0x1, 0x00, 0x00, 0x00,  // Network #1
+                            0x00,                   // Location & speed
+                            (byte)latitude[0],
+                            (byte)latitude[1],
+                            (byte)latitude[2],
+                            (byte)latitude[3],
+                            (byte)longitude[0],
+                            (byte)longitude[1],
+                            (byte)longitude[2],
+                            (byte)longitude[3],
+                            (byte)speed[0],
+                            (byte)speed[1],
+                            (byte)speed[2],
+                            (byte)speed[3],
+                    });
 
                     me.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
                 } catch (Exception e) {
@@ -80,42 +108,104 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-        Thread networkThread = new Thread(new Runnable() {
+        Thread bluetoothThread = new Thread(new Runnable() {
             public void run() {
                 while (true) {
                     try {
-                        Socket s = new Socket();
-                        s.connect(new InetSocketAddress("192.168.4.1", 2228), 5000);
-                        s.setSoTimeout(5000);
-                        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-                        while (true) {
-                            if (in.ready()) {
-                                String input = in.readLine();
+                        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-                                try {
-                                    JSONObject data = new JSONObject(input);
-                                    final double latitude = data.getDouble("latitude");
-                                    final double longitude = data.getDouble("longitude");
+                        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+                        BluetoothDevice device = null;
+                        for (BluetoothDevice pairedDevice : pairedDevices) {
+                            if (pairedDevice.getName().startsWith("VPU "))
+                            {
+                                device = pairedDevice;
+                                break;
+                            }
+                        }
+                        if (device == null)
+                        {
+                            throw new Exception("No paired VPU found");
+                        }
 
-                                    Handler mainHandler = new Handler(Looper.getMainLooper());
-                                    Runnable myRunnable = new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            he.setPosition(new LatLng(latitude, longitude));
+                        String deviceAddress[] = device.getAddress().split(":");
+
+
+                        Method m = device.getClass().getMethod("createInsecureRfcommSocket", new Class[] { int.class });
+                        BluetoothSocket s = (BluetoothSocket)m.invoke(device, Integer.valueOf(1));
+                        s.connect();
+                        try {
+                            InputStream in = s.getInputStream();
+                            OutputStream out = (s.getOutputStream());
+
+                            out.write(new byte[]{
+                                    0x00, // Set networks
+                                    0x1, 0x00, 0x00, 0x00, // Network #1
+                                    (byte)Integer.parseInt(deviceAddress[2], 16),
+                                        (byte)Integer.parseInt(deviceAddress[3], 16),
+                                        (byte)Integer.parseInt(deviceAddress[4], 16),
+                                        (byte)Integer.parseInt(deviceAddress[5], 16),
+                                        // Device address #1
+                            });
+                            out.flush();
+
+                            while (true) {
+                                if (in.available() > 0) {
+                                    in.mark(258);
+                                    int size;
+                                    byte packet[] = new byte[256];
+                                    while (true)
+                                    {
+                                        byte c_size[] = new byte[2];
+                                        if (in.read(c_size, 0, 2) != 2)
+                                        {
+                                            in.reset();
+                                            continue;
                                         }
-                                    };
-                                    mainHandler.post(myRunnable);
-                                } catch (Exception e) {
-                                    e.printStackTrace();
+                                        size = (int)c_size[0] + (int)c_size[1] * 256;
+                                        if (in.read(packet, 0, size) != size)
+                                        {
+                                            in.reset();
+                                            continue;
+                                        }
+                                        break;
+                                    }
+
+                                    ByteBuffer buffer = ByteBuffer.wrap(packet);
+                                    buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+                                    byte btPacketTypeBuf[] = new byte[1];
+                                    buffer.get(btPacketTypeBuf, 0, 1);
+                                    if (btPacketTypeBuf[0] == 0) {
+                                        int src = buffer.getInt();
+
+                                        byte packetTypeBuf[] = new byte[1];
+                                        buffer.get(packetTypeBuf, 0, 1);
+                                        if (packetTypeBuf[0] == 0) {
+                                            final float latitude = buffer.getFloat();
+                                            final float longitude = buffer.getFloat();
+                                            float speed = buffer.getFloat();
+
+                                            Handler mainHandler = new Handler(Looper.getMainLooper());
+                                            Runnable myRunnable = new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    he.setPosition(new LatLng(latitude, longitude));
+                                                }
+                                            };
+                                            mainHandler.post(myRunnable);
+                                        }
+                                    }
+                                }
+
+                                byte value[] = outputQueue.poll();
+                                if (value != null) {
+                                    out.write(value);
+                                    out.flush();
                                 }
                             }
-
-                            String value = outputQueue.poll();
-                            if (value != null) {
-                                out.write(value.concat("\n"));
-                                out.flush();
-                            }
+                        } finally {
+                            s.close();
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -123,7 +213,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         });
-        networkThread.start();
+        bluetoothThread.start();
     }
 
 
